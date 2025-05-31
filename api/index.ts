bdffcfcae5cee988a2ced8373387b77c4e3cc51e
Bassel Halabi <bassel@shipbee.io>
2025-05-31 05:36:18 +0300

    trial 2

--- api/index.ts
+++ api/index.ts
@@ -1,2 +1,15 @@
-import app from "../dist/app.js";
-export default app;
+import App from "../dist/app.js";
+import "reflect-metadata";
+import { OrderController } from "../dist/controllers/order.controller.js";
+import { CitiesController } from "../dist/controllers/cities.controller.js";
+
+const app = new App(
+    [
+    new OrderController(),
+    new CitiesController(),
+    ],
+);
+app.app.get('/test', (req: any, res: any): void => {
+  res.send('Welcome to the API! 🌟');
+});
+app.listen();
