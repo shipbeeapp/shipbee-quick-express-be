f1b8ab8a48587160613e58e90373059c5ea8dd38
Bassel Halabi <bassel@shipbee.io>
2025-08-07 22:35:03 +0300

    remove test from mail

--- src/services/email.service.ts
+++ src/services/email.service.ts
@@ -83,8 +83,8 @@ export function generateOrderHtml(order: CreateOrderDto, totalCost: number, vehi
     const imageUrls = orderDescription?.images || null;
     const images = imageUrls ? imageUrls.map((img: string) => `${env.CLOUDINARY_BASE_URL}${img}`) : []
     const replacements = {
-      recipient: userType === 'admin' ? 'Test admin' : `Test ${order.senderName}`,
-      heading: userType === 'admin' ? 'Test Request Received – <strong>Quick shipBee!</strong>' : 'Your Test Service request has been submitted!',
+      recipient: userType === 'admin' ? 'admin' : `${order.senderName}`,
+      heading: userType === 'admin' ? 'New Request Received – <strong>Quick shipBee!</strong>' : 'Your Service request has been submitted!',
       name: order.senderName,
       email: order.senderEmail,
       phoneNumber: order.senderPhoneNumber,
