a86ea6db70e6bd8e9ae375067d420fa8841c13e9
Bassel Halabi <bassel@shipbee.io>
2025-10-13 12:21:29 +0300

    add driver and vehicle photos

--- src/resource/drivers/driver.resource.ts
+++ src/resource/drivers/driver.resource.ts
@@ -1,4 +1,5 @@
 import { Driver } from '../../models/driver.model.js';
+import { generatePhotoLink } from '../../utils/global.utils.js';
 
 export class DriverResource {
     static toResponse(driver: any): any {
@@ -9,10 +10,25 @@ export class DriverResource {
             status: driver.driver_status,
             signUpStatus: driver.driver_signUpStatus,
             lastActiveAt: driver.driver_updatedAt,
+            dateOfBirth: driver.driver_dateOfBirth,
+            surname: driver.driver_surname,
+            qid: driver.driver_qid,
+            profilePicture: generatePhotoLink(driver.driver_profilePicture),
+            qidFront: generatePhotoLink(driver.driver_qidFront),
+            qidBack: generatePhotoLink(driver.driver_qidBack),
+            licenseFront: generatePhotoLink(driver.driver_licenseFront),
+            licenseBack: generatePhotoLink(driver.driver_licenseBack),
+            licenseExpirationDate: driver.driver_licenseExpirationDate,
             vehicle: {
                 type: driver.vehicle_type,
                 model: driver.vehicle_model,
                 number: driver.vehicle_number,
+                frontPhoto: generatePhotoLink(driver.vehicle_frontPhoto),
+                backPhoto: generatePhotoLink(driver.vehicle_backPhoto),
+                leftPhoto: generatePhotoLink(driver.vehicle_leftPhoto),
+                rightPhoto: generatePhotoLink(driver.vehicle_rightPhoto),
+                registrationFront: generatePhotoLink(driver.vehicle_registrationFront),
+                registrationBack: generatePhotoLink(driver.vehicle_registrationBack),
             },
             ordersCount: Number(driver.orderCount),
         };
--- src/utils/global.utils.ts
+++ src/utils/global.utils.ts
@@ -1,6 +1,12 @@
 import crypto from "crypto";
+import { env } from "../config/environment.js"
 
 // Generate a secure random token
 export function generateToken() {
   return crypto.randomBytes(16).toString("hex");
+}
+
+export function generatePhotoLink(filename: string): string {
+  const baseUrl = env.CLOUDINARY_BASE_URL;
+  return `${baseUrl}${filename}`;
 }
