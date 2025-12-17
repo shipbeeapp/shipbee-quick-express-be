f019ca12930123893710fa7bc9b3ba17bf99b889
Bassel Halabi <bassel@shipbee.io>
2025-12-17 17:28:23 +0200

    fix firebase config

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -2,9 +2,19 @@ import admin from "firebase-admin";
 import fs from "fs";
 import {env} from "../config/environment.js";
 
-const serviceAccount = JSON.parse(
+let serviceAccount;
+
+if (env.FIREBASE_CONFIG_PATH === '/etc/secrets/firebase-account.json') {
+  console.log("🔐 Using Firebase config from mounted secret");
+  serviceAccount = JSON.parse(
     fs.readFileSync(env.FIREBASE_CONFIG_PATH, "utf8")
   );
+} else {
+  console.log("⚠️ Using Firebase config from local environment variable");
+  serviceAccount = JSON.parse(
+      env.FIREBASE_CONFIG_PATH
+  );
+}
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
