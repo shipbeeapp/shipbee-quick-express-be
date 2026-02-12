b711713d37640e379a38455fee24e945602744b9
Bassel Halabi <bassel@shipbee.io>
2026-02-12 14:04:02 +0200

    add upload controller for forced updates

---
+++ src/controllers/upload.controller.ts
@@ -0,0 +1,59 @@
+import { NextFunction, Response, Router, Request } from 'express';
+import {AuthenticatedRequest, authenticationMiddleware} from '../middlewares/authentication.middleware.js';
+import { env } from '../config/environment.js';
+import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
+import fs from 'fs';
+import multer from 'multer';
+import { console } from 'inspector';
+
+export class UploadController {
+  public router: Router = Router();
+  public path = '/upload';
+
+  private s3Client: S3Client;
+  private uploadMiddleware: multer.Multer;
+
+
+  constructor() {
+    this.s3Client = new S3Client({region: env.AWS.REGION});
+    this.uploadMiddleware = multer({storage: multer.memoryStorage()});
+    this.initializeRoutes();
+  }
+
+
+  private initializeRoutes() {
+    this.router.post(
+        `${this.path}/forced-updates`, 
+        authenticationMiddleware, 
+        this.uploadMiddleware.single('file'),
+        this.upload.bind(this)
+    );
+  }
+ 
+  private async upload(req: AuthenticatedRequest, res: Response, next: NextFunction) {
+    try {
+        if (!req.file) {
+            return res.status(400).json({ message: 'No file uploaded' });
+        }
+        if (req.email !== env.ADMIN.EMAIL) {
+            return res.status(403).json({ message: 'Forbidden' });
+        }
+    
+        const s3Key = env.AWS.FILENAME; // static key, overwrites existing file
+        const putCommand = new PutObjectCommand({
+          Bucket: env.AWS.BUCKET_NAME,
+          Key: s3Key,
+          Body: req.file.buffer,
+          ContentType: req.file.mimetype,
+          CacheControl: 'no-store', // ensures mobile app always gets latest
+        });
+    
+        await this.s3Client.send(putCommand);
+
+        res.status(200).json({ message: 'File uploaded successfully' });
+    } catch (error) {    
+        console.error('Error uploading file:', error);
+        res.status(500).json({ message: 'Internal server error' });        
+    }
+  }
+}
