1daeec2d408c547a9b9a034be494672f05db38f6
Bassel Halabi <bassel@shipbee.io>
2026-02-14 17:14:48 +0200

    move init

--- src/models/driverTag.model.ts
+++ src/models/driverTag.model.ts
@@ -1,6 +1,5 @@
 import { Column, Entity, OneToOne, JoinColumn, ManyToOne } from "typeorm";
 import BaseEntity from "./baseEntity.js";
-import { Driver } from "./driver.model.js";
 import { Tag } from "./tag.model.js";
 
 @Entity("driver_tags")
@@ -8,8 +7,9 @@ export class DriverTag extends BaseEntity {
     @ManyToOne(() => Driver, driver => driver.id, { nullable: false })
     @JoinColumn({ name: "driverId" })
     driver: Driver;
-
+    
     @ManyToOne(() => Tag, tag => tag.id, { nullable: false })
     @JoinColumn({ name: "tagId" })
     tag: Tag;
 }
+import { Driver } from "./driver.model.js";
